dacac428f71f363b6f29c3682596125d240cb67e
Eduardo Broetto <eduardo_abg@hotmail.com>
2022-07-05 22:10:48 -0300

    Atualização

--- src/database/migrations/6-create-tablehorario.ts
+++ src/database/migrations/6-create-tablehorario.ts
@@ -2,51 +2,51 @@ import { QueryInterface, DataTypes } from "sequelize";
 export default {
   up: (queryInterface: QueryInterface) => {
     return queryInterface.createTable("horario", {
-        id: {
-            type: DataTypes.INTEGER(),
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        hora_inicio: {
-            type: DataTypes.TIME(),
-        },
-        hora_fim: {
-            type: DataTypes.TIME(),
-        },
-        domingo: {
-            type: DataTypes.BOOLEAN(),
-            defaultValue: false,
-        },
-        segunda: {
-            type: DataTypes.BOOLEAN(),
-            defaultValue: false,
-        },
-        terca: {
-            type: DataTypes.BOOLEAN(),
-            defaultValue: false,
-        },
-        quarta: {
-            type: DataTypes.BOOLEAN(),
-            defaultValue: false,
-        },
-        quinta: {
-            type: DataTypes.BOOLEAN(),
-            defaultValue: false,
-        },
-        sexta: {
-            type: DataTypes.BOOLEAN(),
-            defaultValue: false,
-        },
-        sabado: {
-            type: DataTypes.BOOLEAN(),
-            defaultValue: false,
-        },
-        createdAt: {
-            type: DataTypes.DATE(),
-        },
-        updatedAt: {
-            type: DataTypes.DATE(),
-        },
+      id: {
+        type: DataTypes.INTEGER(),
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      hora_inicio: {
+        type: DataTypes.TIME(),
+      },
+      hora_fim: {
+        type: DataTypes.TIME(),
+      },
+      domingo: {
+        type: DataTypes.BOOLEAN(),
+        defaultValue: false,
+      },
+      segunda: {
+        type: DataTypes.BOOLEAN(),
+        defaultValue: false,
+      },
+      terca: {
+        type: DataTypes.BOOLEAN(),
+        defaultValue: false,
+      },
+      quarta: {
+        type: DataTypes.BOOLEAN(),
+        defaultValue: false,
+      },
+      quinta: {
+        type: DataTypes.BOOLEAN(),
+        defaultValue: false,
+      },
+      sexta: {
+        type: DataTypes.BOOLEAN(),
+        defaultValue: false,
+      },
+      sabado: {
+        type: DataTypes.BOOLEAN(),
+        defaultValue: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE(),
+      },
+      updatedAt: {
+        type: DataTypes.DATE(),
+      },
     });
   },
   down: (queryInterface: QueryInterface) => {

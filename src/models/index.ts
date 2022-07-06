fec73ba802870fb1bed86b1b71d2a3029abef4f0
Eduardo Broetto <eduardo_abg@hotmail.com>
2022-07-05 23:42:55 -0300

    atualização do index do models

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,12 +5,20 @@ import { Horario } from "./Horario";
 import { Servico } from "./Servico";
 import { ConsultorioConvenio } from "./ConsultorioConvenio";
 import { Especialidade } from "./Especialidade";
+import { Profissional } from "./Profissional";
+import { ConsultorioHorario } from "./ConsultorioHorario";
+import { Convenio } from "./Convenio";
+import { Consultorio } from "./Consultorio";
 const sintomas = new Sintomas(mySqlConection);
 const profissao = new Profissao(mySqlConection);
 const horario = new Horario(mySqlConection);
 const servico = new Servico(mySqlConection);
 const consultorioConvenio = new ConsultorioConvenio(mySqlConection);
 const especialidade = new Especialidade(mySqlConection);
+const profissional = new Profissional(mySqlConection);
+const consultorioHorario = new ConsultorioHorario(mySqlConection);
+const convenio = new Convenio(mySqlConection);
+const consultorio = new Consultorio(mySqlConection);
 export {
   sintomas,
   profissao,
@@ -18,4 +26,8 @@ export {
   servico,
   consultorioConvenio,
   especialidade,
+  profissional,
+  consultorioHorario,
+  convenio,
+  consultorio,
 };

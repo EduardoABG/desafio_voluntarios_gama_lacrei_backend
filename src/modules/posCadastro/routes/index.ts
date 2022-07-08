962f2cac81ae68c50955a213da5faafb10202770
Eduardo Broetto <eduardo_abg@hotmail.com>
2022-07-08 10:32:38 -0300

    Pequena alteração

--- src/modules/posCadastro/routes/index.ts
+++ src/modules/posCadastro/routes/index.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { posCadastroController } from "../controllers";
-// import auth from "../../../infra/middlewares/auth";
+import auth from "../../../infra/middlewares/auth";
 
 const routes = Router();
 
-routes.post("/:id", /*auth,*/ posCadastroController.update());
+routes.post("/:id", auth, posCadastroController.update());
 
 export default routes;
